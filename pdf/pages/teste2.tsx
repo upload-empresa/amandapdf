a859acfcc6f38dbfb50d200586de1fef888319f9
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2024-04-05 12:58:50 -0300

    add table alterações

--- pdf/pages/teste2.tsx
+++ pdf/pages/teste2.tsx
@@ -1,23 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ExcelJS from "exceljs";
 
-const toDataURL = (url) => {
-    const promise = new Promise((resolve, reject) => {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            var reader = new FileReader();
-            reader.readAsDataURL(xhr.response);
-            reader.onloadend = function () {
-                resolve({ base64Url: reader.result });
-            };
-        };
-        xhr.open("GET", url);
-        xhr.responseType = "blob";
-        xhr.send();
-    });
 
-    return promise;
-};
 
 const App = () => {
     const [data, setData] = useState([]);
@@ -33,84 +17,38 @@ const App = () => {
 
     const exportExcelFile = () => {
         const workbook = new ExcelJS.Workbook();
-        const sheet = workbook.addWorksheet("My Sheet");
+        // const sheet = workbook.addWorksheet("My Sheet");
+        const sheet = workbook.addWorksheet('My Sheet', {
+            headerFooter: { firstHeader: "Hello Exceljs", firstFooter: "Hello World" }
+        });
+        // Set footer (default centered), result: "Page 2 of 16"
+        sheet.headerFooter.oddFooter = "Page &P of &N";
         sheet.properties.defaultRowHeight = 40;
 
+        // merge a range of cells
+        sheet.mergeCells('A1:Z1');
 
+        // ... merged cells are linked
+        sheet.getCell('A1').value = 'AVALIAÇÃO ERGONÔMICA PRELIMINAR - NR-17 ';
 
-        // Add a new row for the title
-        // const titleRow = sheet.addRow([]);
-        // titleRow.getCell(1).value = "AVALIAÇÃO ERGONÔMICA PRELIMINAR - NR-17 ";
-        // titleRow.getCell(1).alignment = { horizontal: "center" };
-        // titleRow.getCell(1).font = {
-        //     name: "Calibri",
-        //     family: 4,
-        //     size: 14,
-        //     bold: true,
-        //     color: { argb: "FFFFFF" }
-        // };
-        // titleRow.getCell(1).fill = {
-        //     type: "pattern",
-        //     pattern: "solid",
-        //     fgColor: { argb: "1F3864" }
-        // };
-        // sheet.mergeCells("A2:G2");
-        // sheet.getRow(2).height = 30;
-
-        sheet.columns = [
-            {
-                header: "Id",
-                key: "id",
-                width: 20,
-            },
-            { header: "Title", key: "title", width: 32 },
-            {
-                header: "Brand",
-                key: "brand",
-                width: 20,
-            },
-            {
-                header: "Category",
-                key: "category",
-                width: 20,
-            },
-            {
-                header: "Price",
-                key: "price",
-                width: 15,
-            },
-            {
-                header: "Rating",
-                key: "rating",
-                width: 10,
-            },
-
-        ];
-
-        // Add a blank row
-        const title = sheet.addRow({ id: "AVALIAÇÃO ERGONÔMICA PRELIMINAR - NR-17 " });
-        title.alignment = { horizontal: "center" };
-        title.font = {
-            name: "Calibri",
-            family: 4,
-            size: 14,
-            bold: true,
-            color: { argb: "FFFFFF" }
+        sheet.getCell('A1').alignment = { vertical: 'middle', horizontal: 'center' };
+        sheet.getCell('A1').fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor: { argb: '1F3864' },
         };
-        // title.fill = {
-        //     type: "pattern",
-        //     pattern: "solid",
-        //     fgColor: { argb: "1F3864" }
-        // };
-        sheet.mergeCells("A2:G2");
-        sheet.getCell("A2").fill = {
-            type: "pattern",
-            pattern: "solid",
-            fgColor: { argb: "1F3864" }
-        }
+        //testando
+        // for the graduate graphic designers...
+        sheet.getCell('A1').font = {
+            // name: 'Arial Black',
+            color: { argb: 'FFFFFF' },
+            // family: 2,
+            // size: 14,
+            // // italic: true
+        };
+        //testando
 
-
-
+        =SE(E(AE16>=0,1;AE16<=6);'DADOS - MATRIZ DE RISCO'!$L$7;SE(E(AE16>=6,1;AE16<=12);'DADOS - MATRIZ DE RISCO'!$L$6;SE(E(AE16>=12,1;AE16<=32);'DADOS - MATRIZ DE RISCO'!$L$5;SE(E(AE16>=32,1;AE16<=79);'DADOS - MATRIZ DE RISCO'!$L$4;SE(E(AE16>=80;AE16<=160);'DADOS - MATRIZ DE RISCO'!$L$3;"")))))
 
 
         const promise = Promise.all(
